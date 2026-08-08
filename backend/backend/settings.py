98d00a5781f0d4924a20565dd412125cd0626bed
Isha <isha2927@gmail.com>
2026-08-08 14:21:10 +0530

    Prepare backend for Render deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,5 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# CORS
+
 CORS_ALLOW_ALL_ORIGINS = True
